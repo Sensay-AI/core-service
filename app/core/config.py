e43942381583791a6cfbe2dddc58654cb6bf0015
Binh Pham <binh.pham2@mservice.com.vn>
2023-07-18 23:12:25 +0700

    SAI-12:[CoreService] Add Postgre URL

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,6 @@ BASE_DIR = ROOT.parent  # ./
 
 config = Config(BASE_DIR / ".env")
 
-
 API_USERNAME = config("API_USERNAME", str)
 API_PASSWORD = config("API_PASSWORD", str)
 
@@ -17,3 +16,7 @@ API_ALGORITHM = config("API_ALGORITHM", str)
 API_ACCESS_TOKEN_EXPIRE_MINUTES = config(
     "API_ACCESS_TOKEN_EXPIRE_MINUTES", int
 )  # infinity
+
+DATABASE_URL = "postgresql://lie.temp2000:TxBrP8VCe6un@ep-lucky-bar-509389-pooler.us-east-2.aws.neon.tech/neondb"
+API_V1_STR = "/api/v1"
+
