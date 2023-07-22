0243c87d74459b03f6db511c0f4cbc412f592d8a
Binh Pham <binh.pham2@mservice.com.vn>
2023-07-22 09:55:11 +0700

    SAI-12:[CoreService] Update config

--- app/core/config.py
+++ app/core/config.py
@@ -16,9 +16,9 @@ API_ALGORITHM = config("API_ALGORITHM", str)
 API_ACCESS_TOKEN_EXPIRE_MINUTES = config("API_ACCESS_TOKEN_EXPIRE_MINUTES", int)  # infinity
 
 # auth0 conf
-DOMAIN_AUTH0 = "dev-5zgqens2l7jura3i.us.auth0.com"
-API_AUDIENCE = "https://dev-5zgqens2l7jura3i.us.auth0.com/api/v2/"
-ISSUER = "https://dev-5zgqens2l7jura3i.us.auth0.com/"
+DOMAIN_AUTH0 = ""
+API_AUDIENCE = ""
+ISSUER = ""
 ALGORITHMS = "RS256"
 # ------------------
 DATABASE_URL = "postgresql://lie.temp2000:TxBrP8VCe6un@ep-lucky-bar-509389-pooler.us-east-2.aws.neon.tech/neondb"
