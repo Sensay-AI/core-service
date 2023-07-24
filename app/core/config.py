de082b72d5448a04f411e49983a0ac4a546a6d81
Binh Pham <binh.pham2@mservice.com.vn>
2023-07-24 23:24:43 +0700

    SAI-12:[CoreService] Create env value for AUTH variables

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from starlette.config import Config
 
 ROOT = pathlib.Path(__file__).resolve().parent.parent  # app/
 BASE_DIR = ROOT.parent  # ./
-
+API_V1_STR = "/api/v1"
 config = Config(BASE_DIR / ".env")
 
 API_USERNAME = config("API_USERNAME", str)
@@ -13,13 +13,14 @@ API_PASSWORD = config("API_PASSWORD", str)
 # Auth configs.
 API_SECRET_KEY = config("API_SECRET_KEY", str)
 API_ALGORITHM = config("API_ALGORITHM", str)
-API_ACCESS_TOKEN_EXPIRE_MINUTES = config("API_ACCESS_TOKEN_EXPIRE_MINUTES", int)  # infinity
+API_ACCESS_TOKEN_EXPIRE_MINUTES = config(
+    "API_ACCESS_TOKEN_EXPIRE_MINUTES", int
+)  # infinity
 
 # auth0 conf
-DOMAIN_AUTH0 = ""
-API_AUDIENCE = ""
-ISSUER = ""
-ALGORITHMS = "RS256"
+DOMAIN_AUTH0 = config("DOMAIN_AUTH0", str)
+API_AUDIENCE = config("API_AUDIENCE", str)
+AUTH0_ISSUER = config("AUTH0_ISSUER", str)
+AUTH0_ALGORITHMS = config("AUTH0_ALGORITHMS", str)
 # ------------------
-DATABASE_URL = "postgresql://lie.temp2000:TxBrP8VCe6un@ep-lucky-bar-509389-pooler.us-east-2.aws.neon.tech/neondb"
-API_V1_STR = "/api/v1"
+POSTGRESQL_URI = config("POSTGRESQL_URI", str)
