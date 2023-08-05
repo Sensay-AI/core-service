a4be0129c40231076a048b5b3fbdf190fe29b601
tritct <tri.trancongtoan@gmail.com>
2023-08-06 01:30:30 +0700

    fix lint errors

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,12 +1,5 @@
 import logging
-from PIL import Image
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 logger = logging.getLogger(__name__)
-
-
-def get_image_storage_size(image_bytes):
-    with Image.open(image_bytes) as img:
-        storage_size = len(img.fp.read())
-    return storage_size
