3476c22defd0a7e7cc71b3f14df767e12ea1e644
tritct <tri.trancongtoan@gmail.com>
2023-08-08 18:28:00 +0700

    reformat code to resolve comments, fix lint errors

--- app/captions/replicate_caption.py
+++ app/captions/replicate_caption.py
@@ -1,28 +1,29 @@
 from io import BytesIO
+
 import replicate
-from fastapi.responses import StreamingResponse
+from singleton_decorator import singleton
 
 from app.core import config
 from app.utils.utils import logger
 
 
+@singleton
 class CaptionGenerator:
     def __init__(self) -> None:
         self.mplug_model_id = config.IMAGE_CAPTIONING_MODEL
-        self.replicate_generator = replicate.Client(api_token=config.REPLICATE_API_TOKEN)
+        self.replicate_generator = replicate.Client(
+            api_token=config.REPLICATE_API_TOKEN
+        )
 
-    def generate_from_image(self, prompt: str, image_file: BytesIO):
+    def generate_from_image(self, prompt: str, image_file: BytesIO) -> str:
         try:
             output = self.replicate_generator.run(
-                self.mplug_model_id,
-                input={"prompt": prompt,
-                       "img": image_file}
+                self.mplug_model_id, input={"prompt": prompt, "img": image_file}
             )
             caption = []
             for word in output:
                 caption.append(word)
         except Exception as e:
             logger.error(f"Replicate Error {e}", exc_info=True)
-            return None
-        
+            return ""
         return "".join(caption)
