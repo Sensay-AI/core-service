3654b6b5d13a5f0aefd77b6469ae16ca6475dd76
Binh Pham <binh.pham2@mservice.com.vn>
2023-08-10 17:49:20 +0700

    SAI-31:[CoreService] Update test config

--- app/tests/config_test.py
+++ app/tests/config_test.py
@@ -1,32 +1,36 @@
-import requests
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.pool import StaticPool
 # import pytest
+from typing import Generator
+
+import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
 from app.core.config import (
     API_AUDIENCE,
     AUTH_TEST_CLIENT_ID,
     AUTH_TEST_CLIENT_SECRET,
     DOMAIN_AUTH0,
+    POSTGRESQL_URI,
 )
+from app.db.database import Base
 
-# from app.db.database import Base
+engine = create_engine(
+    POSTGRESQL_URI,
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# engine = create_engine(
-#     POSTGRESQL_URI,
-#     poolclass=StaticPool,
-# )
-# TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 #
 #
 # @pytest.fixture(scope="module")
-# def db_session():
-#     Base.metadata.create_all(engine)
-#     session = TestingSessionLocal()
-#     yield session
-#     session.rollback()
-#     session.close()
+def db_session() -> Generator:
+    Base.metadata.create_all(engine)
+    session = TestingSessionLocal()
+    yield session
+    session.rollback()
+    session.close()
 
 
 class MockToken:
