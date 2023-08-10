23a96c166b5163d3c25dbbfeb241a0935de7a148
Binh Pham <binh.pham2@mservice.com.vn>
2023-08-10 16:08:59 +0700

    SAI-31:[CoreService] Create model + crud for vocabulary question

--- app/schemas/vocabulary.py
+++ app/schemas/vocabulary.py
@@ -7,3 +7,20 @@ class GetVocabularyQuestions(BaseModel):
     learning_language: str
     num_questions: int = 5
     num_answers: int = 4
+
+
+class VocabularyAnswerCreate(BaseModel):
+    answer_text: str
+    is_correct: bool
+
+
+class VocabularyQuestionCreate(BaseModel):
+    question_text: str
+    answers: list[VocabularyAnswerCreate]
+
+
+class VocabularyPromptCreate(BaseModel):
+    prompt: str
+    category: str
+    questions: list[VocabularyQuestionCreate]
+    language: str
