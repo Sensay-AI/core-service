f664add553be5a87e804a3f1ac2002a95987f22b
Binh Pham <binh.pham2@mservice.com.vn>
2023-08-10 16:08:59 +0700

    SAI-31:[CoreService] Create model + crud for vocabulary question

---
+++ app/crud/crud_vocabulary.py
@@ -0,0 +1,76 @@
+from http import HTTPStatus
+from typing import Any, Optional
+
+from fastapi import HTTPException
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from app.models.language import Language
+from app.models.vocabulary import (
+    Category,
+    VocabularyAnswer,
+    VocabularyPrompt,
+    VocabularyQuestion,
+)
+from app.schemas.vocabulary import (
+    VocabularyPromptCreate,
+)
+
+
+class CRUDVocabularyPrompt(CRUDBase[VocabularyPrompt, VocabularyPromptCreate, Any]):
+    def create_with_category(
+        self, db: Session, vocabularyPromptCreate: VocabularyPromptCreate, user_id: str
+    ) -> Any:
+        stmt = (
+            insert(Category)
+            .values(category_name=vocabularyPromptCreate.category, user_id=user_id)
+            .on_conflict_do_update(
+                index_elements=["category_name", "user_id"],
+                set_={"category_name": vocabularyPromptCreate.category},
+            )
+            .returning(Category.id)
+        )
+
+        result = db.execute(stmt)
+        insert_id = result.fetchone()[0]
+
+        language: Optional[Language] = (
+            db.query(Language)
+            .filter(Language.language_name == vocabularyPromptCreate.language.upper())
+            .first()
+        )
+
+        if not language:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail=f"{vocabularyPromptCreate.language.upper()} does not support yet!",
+            )
+
+        vocabulary_prompt = VocabularyPrompt(
+            prompt=vocabularyPromptCreate.prompt,
+            category_id=insert_id,
+            language_id=language.id,
+        )
+
+        questions = []
+        answers = []
+        for question in vocabularyPromptCreate.questions:
+            vocabulary_question = VocabularyQuestion(
+                question_text=question.question_text,
+                prompt=vocabulary_prompt,
+                language_id=language.id,
+            )
+            questions.append(vocabulary_question)
+            for answer in question.answers:
+                vocabulary_answer = VocabularyAnswer(
+                    answer_text=answer.answer_text,
+                    question=vocabulary_question,
+                    is_correct=answer.is_correct,
+                    language_id=language.id,
+                )
+                answers.append(vocabulary_answer)
+        db.add(vocabulary_prompt)
+        db.add_all(questions)
+        db.add_all(answers)
+        db.commit()
--- app/routes/api_v1/endpoints/vocabulary.py
+++ app/routes/api_v1/endpoints/vocabulary.py
@@ -3,30 +3,103 @@ from __future__ import annotations
 from typing import Any
 
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from app.core.auth0 import check_user
+from app.crud.crud_vocabulary import CRUDVocabularyPrompt
+from app.db.database import get_db
 from app.llm.vocabulary import ChatGPTVocabularyGenerator
+from app.models.vocabulary import Category, VocabularyPrompt
 from app.schemas.users import Auth0User
-from app.schemas.vocabulary import GetVocabularyQuestions
+from app.schemas.vocabulary import (
+    GetVocabularyQuestions,
+    VocabularyAnswerCreate,
+    VocabularyPromptCreate,
+    VocabularyQuestionCreate,
+)
 
 router = APIRouter()
 
 
+def _parse_json_prompt(
+    category: str, language: str, data: dict
+) -> VocabularyPromptCreate:
+    questions = []
+    data = data[language]
+    for question in data["questions"]:
+        answers = []
+        for answer in question["options"]:
+            is_correct = answer == question["answer"]
+            answers.append(
+                VocabularyAnswerCreate(answer_text=answer, is_correct=is_correct)
+            )
+
+        questions.append(
+            VocabularyQuestionCreate(
+                question_text=question["question"], answers=answers
+            )
+        )
+
+    return VocabularyPromptCreate(
+        prompt=data["lesson"],
+        category=category,
+        questions=questions,
+        language=language,
+    )
+
+
+def generate_data() -> VocabularyPromptCreate:
+    answers = [
+        VocabularyAnswerCreate(answer_text="1", is_correct=False),
+        VocabularyAnswerCreate(answer_text="2", is_correct=False),
+        VocabularyAnswerCreate(answer_text="3", is_correct=False),
+        VocabularyAnswerCreate(answer_text="4", is_correct=True),
+    ]
+    question = VocabularyQuestionCreate(question_text="ABCDEFGH?", answers=answers)
+    return VocabularyPromptCreate(
+        prompt="this is sample prompt 2",
+        category="movie",
+        questions=[question],
+        language="english",
+    )
+
+
 @router.get("/")
-async def get(*, auth: Auth0User = Depends(check_user)) -> dict[str, Any]:
+async def get(*, db: Session = Depends(get_db)) -> dict[str, Any]:
+    abc = CRUDVocabularyPrompt(VocabularyPrompt)
+    abc.create_with_category(db, generate_data(), "facebook|4152771494955357")
     return {"message": "successfully"}
 
 
-@router.post("/")
-async def getVocabularyQuestions(
-    *, user_input: GetVocabularyQuestions, _: Auth0User = Depends(check_user)
+@router.post("/questions")
+async def get_new_vocabulary_questions(
+    *,
+    user_input: GetVocabularyQuestions,
+    db: Session = Depends(get_db),
+    auth: Auth0User = Depends(check_user),
 ) -> dict[str, Any]:
-    return {
-        "data": ChatGPTVocabularyGenerator().generateVocabularyQuestion(
-            category=user_input.category,
-            primary_language=user_input.primary_language,
-            learning_language=user_input.learning_language,
-            num_questions=user_input.num_questions,
-            num_answers=user_input.num_answers,
-        )
-    }
+    questions: dict[str, Any] = ChatGPTVocabularyGenerator().generateVocabularyQuestion(
+        category=user_input.category,
+        primary_language=user_input.primary_language,
+        learning_language=user_input.learning_language,
+        num_questions=user_input.num_questions,
+        num_answers=user_input.num_answers,
+    )
+
+    # primary_language_object = _parse_json_prompt(user_input.category, user_input.primary_language, questions)
+    learning_language_object = _parse_json_prompt(
+        user_input.category, user_input.learning_language, questions
+    )
+    CRUDVocabularyPrompt(VocabularyPromptCreate).create_with_category(
+        db, learning_language_object, auth.id
+    )
+    return {"items": questions}
+
+
+@router.get("/categories")
+async def get_user_categories(
+    *, db: Session = Depends(get_db), auth: Auth0User = Depends(check_user)
+) -> dict[str, Any]:
+    categories = db.query(Category).filter(Category.user_id == auth.id).limit(100).all()
+    categories = [category.__dict__ for category in categories]
+    return {"items": categories}
