cf77c77adfb301d4b2ecaf92081bc8dd558dc55d
Minh NgLD <66552054+minhnld@users.noreply.github.com>
2023-08-11 19:28:15 +0700

    SAI-12:[CoreService] REFACTOR update profile   (#15)
    * Update "update user profile" api
    
    * refactor unit test
    
    * make lint
    
    * update unittest

--- app/routes/api_v1/endpoints/user.py
+++ app/routes/api_v1/endpoints/user.py
@@ -15,17 +15,23 @@ from app.schemas.users import Auth0User, UserUpdate
 router = APIRouter()
 
 
+async def query_user_info(
+    auth: Auth0User = Depends(check_user), db: Session = Depends(get_db)
+) -> Optional[UserInfo]:
+    user: Optional[UserInfo] = (
+        db.query(UserInfo).filter(UserInfo.user_id == auth.id).first()
+    )
+    return user
+
+
 @router.get("/")
 async def get_user_profile(
     *, db: Session = Depends(get_db), auth: Auth0User = Depends(check_user)
 ) -> dict[str, Any]:
-    user: Optional[UserInfo] = (
-        db.query(UserInfo).filter(UserInfo.user_id == auth.id).first()
-    )
-
+    user = await query_user_info(auth, db)
     if not user:
         raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST,
+            status_code=HTTPStatus.NOT_FOUND,
             detail="The user does not exists!",
         )
     return user.__dict__
@@ -37,14 +43,11 @@ async def create_user_profile(
     user_input: UserUpdate,
     db: Session = Depends(get_db),
     auth: Auth0User = Depends(check_user),
-) -> dict[str, str]:
-    user: Optional[UserInfo] = (
-        db.query(UserInfo).filter(UserInfo.user_id == auth.id).first()
-    )
-
+) -> dict[str, Any]:
+    user = await query_user_info(auth, db)
     if user:
         raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST,
+            status_code=HTTPStatus.NOT_FOUND,
             detail="The user already exists!",
         )
     try:
@@ -57,10 +60,13 @@ async def create_user_profile(
         user.phone_number = user_input.phone_number
         user.nickname = user_input.nickname
         user.date_of_birth = user_input.date_of_birth
+        user.gender = "" if user_input.gender is None else user_input.gender.name
+        user.picture = user_input.picture
         db.add(user)
         db.commit()
         db.refresh(user)
-        return {"message": "Create user successfully!"}
+        created_user = await query_user_info(auth, db)
+        return created_user.__dict__
     except SQLAlchemyError as e:
         db.rollback()
         raise HTTPException(
@@ -74,14 +80,11 @@ async def update_user_profile(
     user_input: UserUpdate,
     db: Session = Depends(get_db),
     auth: Auth0User = Depends(check_user),
-) -> dict[str, str]:
-    user: Optional[UserInfo] = (
-        db.query(UserInfo).filter(UserInfo.user_id == auth.id).first()
-    )
-
+) -> dict[str, Any]:
+    user = await query_user_info(auth, db)
     if not user:
         raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST,
+            status_code=HTTPStatus.NOT_FOUND,
             detail="The user does not exists!",
         )
     try:
@@ -92,11 +95,12 @@ async def update_user_profile(
         user.phone_number = user_input.phone_number
         user.nickname = user_input.nickname
         user.date_of_birth = user_input.date_of_birth
-
+        user.gender = "" if user_input.gender is None else user_input.gender.name
+        user.picture = user_input.picture
         db.commit()
         db.refresh(user)
-        return {"message": "Update user successfully!"}
-
+        updated_user_profile = await query_user_info(auth, db)
+        return updated_user_profile.__dict__
     except SQLAlchemyError as e:
         db.rollback()
         raise HTTPException(
--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -7,6 +7,8 @@ from app.main import app
 from app.schemas.users import Auth0User
 from app.tests.config_test import MockToken
 
+APPLICATION_JSON = "application/json"
+
 client = TestClient(app)
 token = MockToken.get_token()
 
@@ -15,7 +17,7 @@ def test_get_user_info_without_token():
     response = client.get(
         "/api/v1/user",
         headers={
-            "Accept": "application/json",
+            "Accept": APPLICATION_JSON,
         },
     )
     assert response.status_code == HTTPStatus.FORBIDDEN
@@ -25,7 +27,7 @@ def test_get_user_info():
     response = client.get(
         "/api/v1/user",
         headers={
-            "Accept": "application/json",
+            "Accept": APPLICATION_JSON,
             "Authorization": "Bearer " + token,
         },
     )
@@ -33,16 +35,16 @@ def test_get_user_info():
 
 
 def test_user_scope_found():
-    auth0User: Auth0User = Auth0Helper.verify_token(token)
-    assert len(auth0User.permissions) != 0
-    assert "create:client_grants" in auth0User.permissions
+    auth0_user: Auth0User = Auth0Helper.verify_token(token)
+    assert len(auth0_user.permissions) != 0
+    assert "create:client_grants" in auth0_user.permissions
 
 
 def test_create_user():
     resp = client.post(
         "/api/v1/user/create",
         headers={
-            "Accept": "application/json",
+            "Accept": APPLICATION_JSON,
             "Authorization": "Bearer " + token,
         },
         json={
@@ -55,4 +57,4 @@ def test_create_user():
             "date_of_birth": "2005-07-26",
         },
     )
-    assert resp.status_code in (HTTPStatus.OK, HTTPStatus.BAD_REQUEST)
+    assert resp.status_code in (HTTPStatus.OK, HTTPStatus.NOT_FOUND)
