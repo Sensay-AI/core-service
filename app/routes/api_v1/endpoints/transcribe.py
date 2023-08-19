8e67a65ed1b1f5db041a2c8042ac70d7bb9be552
q15825_canon <60034216+hphung10499@users.noreply.github.com>
2023-08-19 21:11:51 +0700

    [Phung] Fix linting check

--- app/routes/api_v1/endpoints/transcribe.py
+++ app/routes/api_v1/endpoints/transcribe.py
@@ -1,16 +1,17 @@
 """ Transcribe api. """
-import io
 import base64
-import json
+from http import HTTPStatus
 
 from google.cloud.speech_v2 import SpeechClient
 from google.cloud.speech_v2.types import cloud_speech
 
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
-from app.schemas.users import Auth0User
-from app.container.containers import Container
 from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
+
+from app.container.containers import Container
 from app.routes.api_v1.endpoints.auth import check_user
+from app.schemas.users import Auth0User
+from app.infrastructure.aws.s3 import S3Service
 
 
 #Router initialize
@@ -19,7 +20,7 @@ router = APIRouter()
 @router.post("/")
 async def transcribe_audio (
     language_code: str,
-    file: UploadFile, 
+    file: UploadFile,
     auth: Auth0User = Depends(check_user),
     project_id: str = Depends(Provide[Container.google_project_id]),
     gcp_credentials: str = Depends(Provide[Container.google_credential_file]),
@@ -28,35 +29,37 @@ async def transcribe_audio (
 
     decoded_gcp_credentials = base64.b64decode(gcp_credentials).decode("utf-8")
     temp_credentials_path = "temp_credentials.json"
-    with open(temp_credentials_path, "w") as temp_file:
+    with open(temp_credentials_path,  "w", encoding="utf-8") as temp_file:
         temp_file.write(decoded_gcp_credentials)
 
-    audioFileType = file.content_type
+    audio_file_type = file.content_type
     # Connect to Google Cloud Platform file
     client = SpeechClient.from_service_account_file(temp_credentials_path)
 
     # Check if the uploaded file is an audio mp3 file
-    if not ( audioFileType.startswith("audio/mpeg") or audioFileType == "audio/flac" or audioFileType == "audio/wav" ):
+    if audio_file_type not in  ["audio/mpeg", "audio/flac", "audio/wav"]:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Unsupported file type"
         )
-    
     # Reads a file as bytes
     audio_content = await file.read()
 
     config = cloud_speech.RecognitionConfig(
         auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
         language_codes=[language_code],
-        model="long", 
+        model="long",
         # Reference: https://cloud.google.com/speech-to-text/v2/docs/transcription-model
         features=cloud_speech.RecognitionFeatures(
             enable_automatic_punctuation=True,
         ),
     )
 
+    recogniser_gcp = f"projects/{project_id}/locations/global/recognizers/_".format(
+        project_id = project_id
+        )
     request = cloud_speech.RecognizeRequest(
-        recognizer="projects/{project_id}/locations/global/recognizers/_",
+        recognizer=recogniser_gcp,
         config=config,
         content=audio_content,
     )
@@ -65,4 +68,6 @@ async def transcribe_audio (
     response = client.recognize(request=request)
     transcriptions = [result.alternatives[0].transcript for result in response.results]
 
-    return {"transcriptions": ''.join(transcriptions)}
+    response_transcribe = ''.join(transcriptions)
+
+    return {"transcriptions": response_transcribe}
