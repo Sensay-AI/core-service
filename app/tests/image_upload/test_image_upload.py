429b1cc26e9db3fb68c69ef86e184e3969ba4f2f
minhnguyen <minh.nldang@gmail.com>
2023-08-25 14:51:11 +0700

    NO-TICKET: update test

--- app/tests/image_upload/test_image_upload.py
+++ app/tests/image_upload/test_image_upload.py
@@ -5,7 +5,11 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app.infrastructure.auth0.auth0 import Auth0Service
-from app.infrastructure.aws.s3 import S3Service, UploadS3FileResponse
+from app.infrastructure.aws.s3 import (
+    S3FilesInFolderResponse,
+    S3Service,
+    UploadS3FileResponse,
+)
 from app.main import app
 from app.models.schemas.users import Auth0User
 
@@ -36,7 +40,7 @@ def test_upload_image(client):
         image_bytes = image_file.read()
         image_file = ("test_image.png", image_bytes, "image/png")
         response = client.post(
-            "/api/v1/image/upload",
+            "/api/v1/image",
             files={"image_file": image_file},
             headers={
                 "Accept": APPLICATION_JSON,
@@ -54,4 +58,49 @@ def test_upload_image(client):
         bucket_name=mock.ANY,
         user_id="user123",  # Set the user_id based on the mock
         extension=".png",
+    )
+
+
+def test_get_images(client):
+    auth_service_mock = mock.Mock(spec=Auth0Service)
+    auth_service_mock.verify_token.return_value = Auth0User(
+        sub="user123", permissions=["read", "write"]
+    )
+    s3_service_mock = mock.Mock(spec=S3Service)
+    s3_service_mock.list_s3_files_in_folder.return_value = [
+        S3FilesInFolderResponse(
+            "s3/bucket/path/key1", "http://s31.image.jng", "23/3/2023"
+        ),
+        S3FilesInFolderResponse(
+            "s3/bucket/path/key2", "http://s32.image.jng", "24/3/2023"
+        ),
+    ]
+    app.container.auth.override(auth_service_mock)
+    app.container.s3_service.override(s3_service_mock)
+
+    response = client.get(
+        "/api/v1/image",
+        headers={
+            "Accept": APPLICATION_JSON,
+            "Authorization": "Bearer xyz",
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.json()["items"] == [
+        {
+            "full_url": "http://s31.image.jng",
+            "s3_bucket_path_key": "s3/bucket/path/key1",
+            "last_modified": "23/3/2023",
+        },
+        {
+            "full_url": "http://s32.image.jng",
+            "s3_bucket_path_key": "s3/bucket/path/key2",
+            "last_modified": "24/3/2023",
+        },
+    ]
+
+    s3_service_mock.list_s3_files_in_folder.assert_called_once_with(
+        bucket_name=mock.ANY,
+        user_id="user123",  # Set the user_id based on the mock
     )
